a7fd51c465df5ffaf250628ecf961b8b2b115b54
Yunus Alıcı <yunusac51@gmail.com>
2022-06-12 19:36:31 +0300

    data updated

--- components/intro.js
+++ components/intro.js
@@ -1,5 +1,5 @@
-import data from '../contentrain/Intro/Intro.json'
-const introData = data[0]
+import data from "../contentrain/Intro/Intro.json";
+const introData = data[0];
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
@@ -7,15 +7,8 @@ export default function Intro() {
         {introData.title}
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        {introData.text}{' '}
-        <a
-          href={introData.href}
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-           {introData.underlinetext}
-        </a>{' '}
-        and {introData.cmsname}.
+        {introData.text}
       </h4>
     </section>
-  )
+  );
 }
